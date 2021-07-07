027a9d62120c7be55e82603a087b82d028e1e972
felipe <december1294@gmail.com>
2021-07-07 16:51:09 -0400

    Searchbar is working, now to refine and add filter

--- react-app/src/components/Splash/Splash.js
+++ react-app/src/components/Splash/Splash.js
@@ -4,29 +4,44 @@ import { useParams } from "react-router-dom";
 import { getBusinesses } from '../../store/business'
 function Splash() {
 
-    const [search, setSearch] = useState(' ');
-    const [businesses, setBusinesses] = useState([])
-    const [searchResults, setSearchResults] = useState([])
+  const [search, setSearch] = useState('');
+  const [businesses, setBusinesses] = useState([])
+  const [searchResults, setSearchResults] = useState([])
+  console.log(businesses)
+  console.log(searchResults)
+  const handleSubmit = (e) => {
+    e.preventDefault();
     console.log(businesses)
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(businesses)
-        let temp = [];
-        businesses.forEach(el => {
-            console.log(el)
-            console.log(Object.values(el))
-            if(Object.values(el).includes(search)){
-                temp.append(el)
-            }
-            setSearchResults(temp)
-        })
-        // setSearchResults(businesses.filter(el => el.includes(search)));
+    let temp = [];
+    businesses.forEach(el => {
+      console.log(el)
+      console.log(Object.values(el))
+      let tempArr = [...Object.values(el)];
+      console.log(tempArr)
+      tempArr = tempArr.filter(el => typeof (el) === "string");
+      console.log(tempArr)
+      console.log(search)
+      for (let i = 0; i < tempArr.length; i++) {
+        console.log(tempArr[i])
+
+        if (tempArr[i].toLowerCase().indexOf(search.toLowerCase()) !== -1) {
+          temp.push({...el})
+          console.log(temp)
+        }
+
+
+      }
+
     }
 
-    const handleChange = (e) => {
-        setSearch(e.target.value);
-    }
-
+    )
+    setSearchResults([...temp])
+    console.log(searchResults)
+    // setSearchResults(businesses.filter(el => el.includes(search)));
+  }
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
   useEffect(() => {
     async function fetchData() {
       const response = await fetch("/api/business/");
@@ -36,24 +51,26 @@ function Splash() {
     fetchData();
   }, []);
 
-    {return businesses?(
-        <div>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor="search">Search</label>
-              <input
-                name="search"
-                type="text"
-                placeholder="search"
-                value={search}
-                onChange={handleChange}
-              />
-              <button type='submit'>Submit</button>
-            </div>
-          </form>
-          {searchResults.map((business, idx) => <div key={idx}>  {business.business_name} </div>)}
-        </div>
-    ): (<div>Loading...</div>)}
+  {
+    return businesses ? (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="search">Search</label>
+            <input
+              name="search"
+              type="text"
+              placeholder="search"
+              value={search}
+              onChange={handleChange}
+            />
+            <button type='submit'>Submit</button>
+          </div>
+        </form>
+        {searchResults.map((business, idx) => <div key={idx}>  {business.business_name} </div>)}
+      </div>
+    ) : (<div>Loading...</div>)
+  }
 
 }
 export default Splash;
